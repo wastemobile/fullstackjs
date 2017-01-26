1156d725dd0b3355ea7d0fcd2c880e611f6c6055
wastemobile <wastemobile@me.com>
2017-01-27 02:12:40 +0800

    gulp flow

--- README.md
+++ README.md
@@ -5,9 +5,27 @@
 ### 準備工作
 
 1. 一台 Mac 電腦。
-2. 已安裝 Homebrew，並以 NVM 安裝好 Node、升級完 NPM。
+2. 已安裝 Homebrew，並以 NVM 安裝好 Node、升級完 NPM，當然也安裝了 Git。
 3. 安裝必要的全域套件：
   - `npm install -g express-generator`
   - `npm install -g gulp-cli`
 4. 安裝 MongoDB 資料庫：
   - brew install mongodb
+
+### 專案建立
+
+使用 `express --hbs --view hbs --git fullstackjs` 建立新專案，根據指示 `cd fullstackjs && npm install` 會安裝完相依套件。
+
+Express 預設的模板語言是 jade，改用 Handlebars；使用 `--git` 參數建立時會提供 express 預設的 `.gitignore` 檔案，便於 git 使用。
+
+在終端機輸入 `$ DEBUG=fullstackjs:* npm start` 立即可啟用搭建好的 express 網站，預設是使用瀏覽器開啟 `http://localhost:3000`。
+
+此時修改後端（伺服器）檔案需要 Ctrl + C 停止程式、再重新啟動一次，修改前端檔案也得手動重載瀏覽器，相當不便利，因此接下來要將本地開發環境加上自動化更新、重載與重啟的機制。
+
+### 開發流程配置
+
+後端代碼更新後自動重啟是透過 [nodemon](https://nodemon.io)，前端則是 [browser-sync](https://browsersync.io)。原書中的方式是修改 Node 啟動時的 script，以及修改應用程式主檔 app.js，我則希望全數改以 gulp 處理；同時也不打算如教程中安裝 `node-sass-middleware`，這類前置編譯工作沒必要擺到主機上進行。
+
+安裝必要套件：`npm install -D gulp gulp-nodemon browser-sync`，之後編寫 gulpfile.js 即可。
+
+進行本地開發時，在專案目錄下輸入 `gulp`。
---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var bs = require('browser-sync').create();
+var exec = require('child_process').exec;
+
+// gulp.task('mongo', function (cb) {
+//   exec('mongod --config /usr/local/etc/mongod.conf', function (err, stdout, stderr) {
+//     console.log(stdout);
+//     console.log(stderr);
+//     cb(err);
+//   });
+// });
+
+// our browser-sync config + nodemon chain
+gulp.task('browser-sync', ['nodemon'], function() {
+	bs.init(null, {
+		proxy: "http://localhost:3000",
+		port: 4000,
+	});
+});
+
+// give nodemon time to restart
+gulp.task('bs-delay', function () {
+  setTimeout(function () {
+    bs.reload({ stream: false });
+  }, 1000);
+});
+
+// our gulp-nodemon task
+gulp.task('nodemon', function (cb) {
+	var started = false;
+	return nodemon({
+		script: './bin/www',
+		ext: 'js',
+		ignore: ['public/**/*.js'],
+		env: {
+			'NODE_ENV': 'development',
+			'DEBUG': 'test:*'
+	 }
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true;
+		}
+	})
+	.on('crash', function() {
+		console.log('nodemon.crash');
+	})
+	// .on('restart', function() {
+	// 	//console.log('nodemon.restart');
+	// })
+	.on('restart', function onRestart() {
+		setTimeout(function () {
+	    bs.reload({ stream: false });
+	  }, 1000);
+	})
+	.once('quit', function () {
+		// handle ctrl+c without a big weep
+		process.exit();
+	});
+});
+
+// the real stuff
+gulp.task('default', ['mongo', 'browser-sync'], function () {
+	gulp.watch('./views/**/*.hbs', bs.reload);
+	gulp.watch('./public/**/*.js', bs.reload);
+	gulp.watch('./public/**/*.css', bs.reload);
+	gulp.watch(['./routes/**/*.js', './app.js', './bin/www'], ['bs-delay']);
+});
